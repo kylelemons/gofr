96077fccc33c729b44eb8263b70e0f4a66d5fb23
Kyle Lemons <kyle@kylelemons.net>
2013-02-24 09:52:29 +0000

    tentative benchmarking indicates the trie is much faster

--- trie/trie.go
+++ trie/trie.go
@@ -2,13 +2,17 @@ package trie
 
 import (
 	"net/http"
+	pathpkg "path"
 	"strings"
 )
 
-func pieces(full string, cutset string) []string {
-	return strings.FieldsFunc(full, func(r rune) bool {
-		return strings.IndexRune(cutset, r) >= 0
-	})
+func splitFields(full string, s string) []string {
+	full = strings.TrimPrefix(full, s)
+	full = strings.TrimSuffix(full, s)
+	if full == "" {
+		return nil
+	}
+	return strings.Split(full, s)
 }
 
 type domainPiece struct {
@@ -37,9 +41,45 @@ func (t *domainTrie) find(segs []string) (int, *pathTrie) {
 	return 0, t.leaf
 }
 
-type pathTrie struct{}
+type pathTrie struct {
+	child [28]*pathTrie
+	leaf  http.Handler
+}
+
+func runeIdx(r rune) int {
+	if r == '/' {
+		return 26
+	}
+	if r >= 'A' && r <= 'Z' {
+		return int(r - 'A')
+	}
+	if r >= 'a' && r <= 'z' {
+		return int(r - 'a')
+	}
+	return 27
+}
+
+func (t *pathTrie) find(path string) (sub int, last http.Handler) {
+	s := strings.TrimPrefix(path, "/")
+	add := len(path) - len(s)
+
+	last = t.leaf
+	for i, r := range s {
+		// TODO(kevlar): handle directories
+		next := t.child[runeIdx(r)]
+		if next == nil {
+			break
+		}
+		t = next
+		if t.leaf != nil {
+			sub, last = i+1, t.leaf
+		}
+	}
+	return add + sub, last
+}
 
 type ServeMux struct {
+	domain domainTrie
 }
 
 func (s *ServeMux) Handle(pattern string, handler http.Handler) {
@@ -49,4 +89,10 @@ func (s *ServeMux) HandleFunc(pattern string, handler func(http.ResponseWriter, 
 	s.Handle(pattern, http.HandlerFunc(handler))
 }
 
-func (s *ServeMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {}
+func (s *ServeMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	domain := splitFields(r.Host, ".")
+	_, path := s.domain.find(domain)
+	clean := pathpkg.Clean(r.URL.Path)
+	_, handler := path.find(clean)
+	handler.ServeHTTP(w, r)
+}
--- trie/trie_test.go
+++ trie/trie_test.go
@@ -9,39 +9,76 @@ import (
 
 type textHandler string
 
-func (textHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {}
+func (s textHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusFound)
+}
 
-func TestPieces(t *testing.T) {
+func TestSplitFields(t *testing.T) {
 	tests := []struct {
 		full, sep string
 		pieces    []string
 	}{
-		{"", ".", []string{}},
-		{".", ".", []string{}},
-		{"...", ".", []string{}},
+		{"", ".", nil},
 		{"example.com.", ".", []string{"example", "com"}},
 	}
 
 	for _, test := range tests {
-		p := pieces(test.full, test.sep)
+		p := splitFields(test.full, test.sep)
 		if got, want := p, test.pieces; !reflect.DeepEqual(got, want) {
-			t.Errorf("pieces(%q, %q) = %q, want %q", test.full, test.sep, got, want)
+			t.Errorf("splitFields(%q, %q) = %q, want %q", test.full, test.sep, got, want)
 		}
 	}
 }
 
-func BenchmarkSplitPieces(b *testing.B) {
+func BenchmarkSplitFields(b *testing.B) {
 	const domain = "sub.example.com."
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		pieces(domain, ".")
+		splitFields(domain, ".")
 	}
 }
 
 var (
-	root           = &pathTrie{}
+	root = &pathTrie{
+		child: [28]*pathTrie{
+			'f' - 'a': {
+				child: [28]*pathTrie{
+					'o' - 'a': {
+						child: [28]*pathTrie{
+							'o' - 'a': {
+								leaf: textHandler("root /foo"),
+							},
+						},
+					},
+				},
+			},
+		},
+		leaf: textHandler("root /"),
+	}
 	com            = &pathTrie{}
-	kylelemons_net = &pathTrie{}
+	kylelemons_net = &pathTrie{
+		child: [28]*pathTrie{
+			'b' - 'a': {
+				child: [28]*pathTrie{
+					'a' - 'a': {
+						child: [28]*pathTrie{
+							'r' - 'a': {
+								leaf: textHandler("kylelemons.net /bar"),
+							},
+						},
+					},
+					'o' - 'a': {
+						child: [28]*pathTrie{
+							'o' - 'a': {
+								leaf: textHandler("kylelemons.net /boo"),
+							},
+						},
+					},
+				},
+			},
+		},
+		leaf: textHandler("kylelemons.net /"),
+	}
 
 	TestTrie = domainTrie{
 		child: []*domainPiece{{
@@ -88,19 +125,60 @@ var (
 			found:  kylelemons_net,
 		},
 	}
+	PathFindTests = []struct {
+		base  *pathTrie
+		path  string
+		count int
+		found string
+	}{
+		{
+			base:  root,
+			path:  "/",
+			count: 1,
+			found: "root /",
+		},
+		{
+			base:  root,
+			path:  "/foo",
+			count: 4,
+			found: "root /foo",
+		},
+		{
+			base:  root,
+			path:  "/foo/bar",
+			count: 4,
+			found: "root /foo",
+		},
+		{
+			base:  kylelemons_net,
+			path:  "/",
+			count: 1,
+			found: "kylelemons.net /",
+		},
+		{
+			base:  kylelemons_net,
+			path:  "/ba",
+			count: 1,
+			found: "kylelemons.net /",
+		},
+		{
+			base:  kylelemons_net,
+			path:  "/bo",
+			count: 1,
+			found: "kylelemons.net /",
+		},
+		{
+			base:  kylelemons_net,
+			path:  "/bar/bar",
+			count: 4,
+			found: "kylelemons.net /bar",
+		},
+	}
 )
-
-var serveMux = http.NewServeMux()
-
-func init() {
-	serveMux.Handle("/", textHandler("default"))
-	serveMux.Handle("example.com/", textHandler("example.com root"))
-	serveMux.Handle("kylelemons.net/", textHandler("kylelemons.net root"))
-}
 
 func TestDomainFind(t *testing.T) {
 	for _, test := range DomainFindTests {
-		count, found := TestTrie.find(pieces(test.domain, "."))
+		count, found := TestTrie.find(splitFields(test.domain, "."))
 		if got, want := count, test.count; got != want {
 			t.Errorf("find(%q).count = %v, want %v", test.domain, got, want)
 		}
@@ -113,7 +191,7 @@ func TestDomainFind(t *testing.T) {
 func BenchmarkDomainFind(b *testing.B) {
 	var domains [][]string
 	for _, test := range DomainFindTests {
-		domains = append(domains, pieces(test.domain, "."))
+		domains = append(domains, splitFields(test.domain, "."))
 	}
 	b.ReportAllocs()
 	b.ResetTimer()
@@ -124,21 +202,111 @@ func BenchmarkDomainFind(b *testing.B) {
 	}
 }
 
-func BenchmarkServeMux(b *testing.B) {
-	var domains []*http.Request
-	for _, test := range DomainFindTests {
-		req, err := http.NewRequest("GET", "http://"+test.domain, nil)
-		if err != nil {
-			panic(err)
+func TestPathFind(t *testing.T) {
+	for _, test := range PathFindTests {
+		count, found := test.base.find(test.path)
+		if got, want := count, test.count; got != want {
+			t.Errorf("find(%q).count = %v, want %v", test.path, got, want)
 		}
-		domains = append(domains, req)
+		if got, want := found, textHandler(test.found); got != want {
+			t.Errorf("find(%q).found = %q, want %q", test.path, got, want)
+		}
+	}
+}
+
+func BenchmarkPathFind(b *testing.B) {
+	var bases []*pathTrie
+	var paths []string
+	for _, test := range PathFindTests {
+		bases = append(bases, test.base)
+		paths = append(paths, test.path)
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i += len(bases) {
+		for j := range bases {
+			bases[j].find(paths[j])
+		}
+	}
+}
+
+func makeBenchmarkData() (hosts, paths []string, reqs []*http.Request) {
+	for _, d := range DomainFindTests {
+		if d.found == nil {
+			continue
+		}
+		for _, p := range PathFindTests {
+			if p.base != d.found {
+				continue
+			}
+
+			req, err := http.NewRequest("GET", p.path, nil)
+			if err != nil {
+				panic(err)
+			}
+			req.Header.Set("Host", d.domain)
+			req.Host = d.domain
+
+			hosts = append(hosts, d.domain)
+			paths = append(paths, p.path)
+			reqs = append(reqs, req)
+		}
+	}
+	return
+}
+
+func TestBenchmarkData(t *testing.T) {
+	hosts, paths, reqs := makeBenchmarkData()
+	mux := http.NewServeMux()
+	for i := range hosts {
+		pattern := hosts[i] + paths[i]
+		mux.Handle(pattern, textHandler(pattern))
+	}
+	trie := &ServeMux{
+		domain: TestTrie,
+	}
+	muxes := []http.Handler{
+		mux, trie,
+	}
+	for _, req := range reqs {
+		for _, h := range muxes {
+			rw := httptest.NewRecorder()
+			h.ServeHTTP(rw, req)
+			if rw.Code != http.StatusFound {
+				t.Errorf("%T %q %q: HTTP %d", h, req.Host, req.URL.Path, rw.Code)
+			}
+		}
+	}
+}
+
+func BenchmarkHTTPMux(b *testing.B) {
+	hosts, paths, reqs := makeBenchmarkData()
+	mux := http.NewServeMux()
+	for i := range hosts {
+		pattern := hosts[i] + paths[i]
+		mux.Handle(pattern, textHandler(pattern))
 	}
 	rw := httptest.NewRecorder()
 	b.ReportAllocs()
 	b.ResetTimer()
-	for i := 0; i < b.N; i += len(domains) {
-		for _, req := range domains {
-			serveMux.ServeHTTP(rw, req)
+	for i := 0; i < b.N; i += len(reqs) {
+		for _, req := range reqs {
+			mux.ServeHTTP(rw, req)
+		}
+	}
+}
+
+func BenchmarkServeMux(b *testing.B) {
+	_, _, reqs := makeBenchmarkData()
+	mux := &ServeMux{
+		domain: TestTrie,
+	}
+	rw := httptest.NewRecorder()
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i += len(reqs) {
+		for _, req := range reqs {
+			mux.ServeHTTP(rw, req)
 		}
 	}
 }
