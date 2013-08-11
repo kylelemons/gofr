3c9c367dccec8d9c21a01603f0259b76ea1b9cba
Kyle Lemons <kyle@kylelemons.net>
2013-08-11 07:47:10 +0000

    Rewrite trie code

--- trie/trie_test.go
+++ trie/trie_test.go
@@ -15,367 +15,400 @@
 package trie
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
 	"testing"
+
+	"github.com/kylelemons/godebug/pretty"
 )
 
 type textHandler string
 
 func (s textHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusFound)
+	fmt.Fprintf(w, "%s", s)
 }
 
-func TestSplitFields(t *testing.T) {
+func TestInsert(t *testing.T) {
+	trie := NewDomain().Trie
 	tests := []struct {
-		full, sep string
-		pieces    []string
+		paths []string
+		leaf  textHandler
+		after *Trie
+		err   error
+	}{{
+		paths: []string{"foo", "baz"},
+		leaf:  "/foo/baz handler",
+		after: &Trie{
+			Child: []*Trie{{
+				Name: "foo",
+				Child: []*Trie{{
+					Name: "baz",
+					Leaf: textHandler("/foo/baz handler"),
+				}},
+			}},
+			Leaf: http.HandlerFunc(http.NotFound),
+		},
+	}, {
+		paths: []string{"foo", "bar"},
+		leaf:  "/foo/bar handler",
+		after: &Trie{
+			Child: []*Trie{{
+				Name: "foo",
+				Child: []*Trie{{
+					Name: "bar",
+					Leaf: textHandler("/foo/bar handler"),
+				}, {
+					Name: "baz",
+					Leaf: textHandler("/foo/baz handler"),
+				}},
+			}},
+			Leaf: http.HandlerFunc(http.NotFound),
+		},
+	}, {
+		paths: []string{"foo", "baz"},
+		leaf:  "/foo/baz handler 2",
+		err:   fmt.Errorf("foo: baz: leaf already exists"),
+	}}
+
+	for idx, test := range tests {
+		err := trie.Insert(test.paths, test.leaf)
+		if err != nil || test.err != nil {
+			if !reflect.DeepEqual(err, test.err) {
+				t.Errorf("%d. insert: %v, want %v", idx, err, test.err)
+			}
+			continue
+		}
+		if cmp := pretty.Compare(trie, test.after); cmp != "" {
+			t.Errorf("%d. after insert:\n%s", idx, cmp)
+		}
+	}
+}
+
+func TestFind(t *testing.T) {
+	foobar := &Trie{
+		Name: "bar",
+		Leaf: textHandler("/foo/bar"),
+	}
+	foobaz := &Trie{
+		Name: "baz",
+		Leaf: textHandler("/foo/baz"),
+	}
+	foo := &Trie{
+		Name:  "foo",
+		Child: []*Trie{foobar, foobaz},
+		Leaf:  textHandler("/foo"),
+	}
+	trie := &Trie{
+		Child: []*Trie{foo},
+		Leaf:  textHandler("/"),
+	}
+
+	tests := []struct {
+		paths []string
+		n     int
+		leaf  textHandler
+	}{{
+		paths: []string{},
+		n:     0,
+		leaf:  "/",
+	}, {
+		paths: []string{"fox"},
+		n:     0,
+		leaf:  "/",
+	}, {
+		paths: []string{"foo"},
+		n:     1,
+		leaf:  "/foo",
+	}, {
+		paths: []string{"foo", "bar"},
+		n:     2,
+		leaf:  "/foo/bar",
+	}, {
+		paths: []string{"foo", "baz"},
+		n:     2,
+		leaf:  "/foo/baz",
+	}}
+
+	for _, test := range tests {
+		n, found := trie.Find(test.paths)
+		if got, want := n, test.n; got != want {
+			t.Errorf("Find(%q).n = %v, want %v", test.paths, got, want)
+		}
+		if got, want := found.Leaf, test.leaf; got != want {
+			t.Errorf("Find(%q) found %q, want %q", test.paths, got, want)
+		}
+	}
+}
+
+func TestHandle(t *testing.T) {
+	mux := NewServeMux()
+	tests := []struct {
+		pattern string
+		handler textHandler
+		err     string // panic message
+		after   *ServeMux
 	}{
-		{"", ".", nil},
-		{"example.com.", ".", []string{"example", "com"}},
+		{
+			pattern: "",
+			err:     `handle pattern "" is not in <domain>/<path> form`,
+		},
+		{
+			pattern: "example.com",
+			err:     `handle pattern "example.com" is not in <domain>/<path> form`,
+		},
+		{
+			pattern: "/foo",
+			handler: "/foo handler",
+			after: &ServeMux{
+				Trie: Trie{
+					Leaf: &Domain{
+						Trie: Trie{
+							Child: []*Trie{{
+								Name: "foo",
+								Leaf: textHandler("/foo handler"),
+							}},
+							Leaf: http.HandlerFunc(http.NotFound),
+						},
+					},
+				},
+			},
+		},
+		{
+			pattern: "/foo",
+			err:     `foo: leaf already exists`,
+		},
+		{
+			pattern: "/dir/",
+			handler: "/dir/ index",
+			after: &ServeMux{
+				Trie: Trie{
+					Leaf: &Domain{
+						Trie: Trie{
+							Child: []*Trie{{
+								Name: "dir",
+								Leaf: http.HandlerFunc(addSlash),
+							}, {
+								Name: "dir/",
+								Leaf: textHandler("/dir/ index"),
+							}, {
+								Name: "foo",
+								Leaf: textHandler("/foo handler"),
+							}},
+							Leaf: http.HandlerFunc(http.NotFound),
+						},
+					},
+				},
+			},
+		},
+		{
+			pattern: "example.com/sub/dir/",
+			handler: "example.com/sub/dir/ index",
+			after: &ServeMux{
+				Trie: Trie{
+					Child: []*Trie{{
+						Name: "com",
+						Child: []*Trie{{
+							Name: "example",
+							Leaf: &Domain{
+								Trie: Trie{
+									Child: []*Trie{{
+										Name: "sub/",
+										Child: []*Trie{{
+											Name: "dir",
+											Leaf: http.HandlerFunc(addSlash),
+										}, {
+											Name: "dir/",
+											Leaf: textHandler("example.com/sub/dir/ index"),
+										}},
+									}},
+									Leaf: http.HandlerFunc(http.NotFound),
+								},
+							},
+						}},
+					}},
+					Leaf: &Domain{
+						Trie: Trie{
+							Child: []*Trie{{
+								Name: "dir",
+								Leaf: http.HandlerFunc(addSlash),
+							}, {
+								Name: "dir/",
+								Leaf: textHandler("/dir/ index"),
+							}, {
+								Name: "foo",
+								Leaf: textHandler("/foo handler"),
+							}},
+							Leaf: http.HandlerFunc(http.NotFound),
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for idx, test := range tests {
+		var err string
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					err = fmt.Sprint(r)
+				}
+			}()
+			mux.Handle(test.pattern, test.handler)
+		}()
+		if err != "" || test.err != "" {
+			if got, want := err, test.err; got != want {
+				t.Errorf("%d. Handle(%q): %q, want %q", idx, test.pattern, got, want)
+			}
+			continue
+		}
+		if cmp := pretty.Compare(mux, test.after); cmp != "" {
+			t.Errorf("%d. after Handle(%q):\n%s", idx, test.pattern, cmp)
+		}
+	}
+}
+
+func TestServeHTTP(t *testing.T) {
+	request := func(method, url string) *http.Request {
+		r, err := http.NewRequest(method, url, nil)
+		if err != nil {
+			t.Fatalf("NewRequest(%q, %q): %s", method, url, err)
+		}
+		return r
+	}
+
+	tests := []struct {
+		desc     string
+		handlers []string
+		req      *http.Request
+		code     int
+		redir    string
+		body     string
+	}{
+		{
+			desc:     "basic",
+			handlers: []string{"/foo"},
+			req:      request("GET", "http://example.com/foo"),
+			code:     200,
+			body:     "/foo handler",
+		},
+		{
+			desc:     "basic with domain",
+			handlers: []string{"example.com/foo"},
+			req:      request("GET", "http://example.com/foo"),
+			code:     200,
+			body:     "example.com/foo handler",
+		},
+		{
+			desc:     "other domain",
+			handlers: []string{"example.com/foo"},
+			req:      request("GET", "http://example.net/foo"),
+			code:     404,
+		},
+		{
+			desc:     "sub domain",
+			handlers: []string{"example.com/foo"},
+			req:      request("GET", "http://www.example.com/foo"),
+			code:     302,
+			redir:    "http://example.com/foo",
+		},
+		{
+			desc:     "sub domain query",
+			handlers: []string{"example.com/foo"},
+			req:      request("GET", "http://www.example.com/foo?q"),
+			code:     302,
+			redir:    "http://example.com/foo?q",
+		},
+		{
+			desc:     "dir",
+			handlers: []string{"/dir/"},
+			req:      request("GET", "/dir/"),
+			code:     200,
+			body:     "/dir/ handler",
+		},
+		{
+			desc:     "file in non-dir",
+			handlers: []string{"/foo"},
+			req:      request("GET", "/foo/sub"),
+			code:     404,
+		},
+		{
+			desc:     "file in dir",
+			handlers: []string{"/dir/"},
+			req:      request("GET", "/dir/foo/bar"),
+			code:     200,
+			body:     "/dir/ handler",
+		},
+		{
+			desc:     "dir redir",
+			handlers: []string{"/dir/"},
+			req:      request("GET", "/dir"),
+			code:     302,
+			redir:    "/dir/",
+		},
+		{
+			desc:     "dir redir query",
+			handlers: []string{"/dir/"},
+			req:      request("GET", "/dir?foo"),
+			code:     302,
+			redir:    "/dir/?foo",
+		},
+		{
+			desc:     "no dir redir",
+			handlers: []string{"/dir", "/dir/"},
+			req:      request("GET", "/dir"),
+			code:     200,
+			body:     "/dir handler",
+		},
+		{
+			desc:     "clean",
+			handlers: []string{"/foo/bar"},
+			req:      request("GET", "/foo/baz/../bar"),
+			code:     301,
+			redir:    "/foo/bar",
+		},
+		{
+			desc:     "clean query",
+			handlers: []string{"/foo/bar"},
+			req:      request("GET", "/foo/baz/../bar?q"),
+			code:     301,
+			redir:    "/foo/bar?q",
+		},
 	}
 
 	for _, test := range tests {
-		p := splitFields(test.full, test.sep)
-		if got, want := p, test.pieces; !reflect.DeepEqual(got, want) {
-			t.Errorf("splitFields(%q, %q) = %q, want %q", test.full, test.sep, got, want)
+		mux := NewServeMux()
+		for _, path := range test.handlers {
+			mux.Handle(path, textHandler(path+" handler"))
+		}
+		w := httptest.NewRecorder()
+		mux.ServeHTTP(w, test.req)
+		if got, want := w.Code, test.code; got != want {
+			t.Errorf("%s: GET %q: %d %s, want %d %s", test.desc, test.req.URL,
+				got, http.StatusText(got), want, http.StatusText(want))
+		}
+		switch w.Code / 100 {
+		case 2:
+			if got, want := w.Body.String(), test.body; got != want {
+				t.Errorf("%s: GET %q: body = %q, want %q", test.desc, test.req.URL, got, want)
+			}
+		case 3:
+			if got, want := w.HeaderMap.Get("Location"), test.redir; got != want {
+				t.Errorf("%s: GET %q: redir = %q, want %q", test.desc, test.req.URL, got, want)
+			}
 		}
 	}
 }
 
-func BenchmarkSplitFields(b *testing.B) {
-	const domain = "sub.example.com."
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		splitFields(domain, ".")
-	}
-}
-
-var (
-	root = &pathTrie{
-		child: [28]*pathTrie{
-			'f' - 'a': {
-				child: [28]*pathTrie{
-					'o' - 'a': {
-						child: [28]*pathTrie{
-							'o' - 'a': {
-								child: [28]*pathTrie{
-									26: {
-										name:  "/foo",
-										leaf:  textHandler("root /foo/"),
-										strip: true,
-									},
-									27: {
-										name: "/foo!",
-										leaf: textHandler("root /foo!"),
-									},
-								},
-								name: "/foo",
-								leaf: textHandler("root /foo"),
-							},
-						},
-					},
-				},
-			},
-		},
-		leaf: textHandler("root /"),
-	}
-	com            = &pathTrie{}
-	kylelemons_net = &pathTrie{
-		child: [28]*pathTrie{
-			'b' - 'a': {
-				child: [28]*pathTrie{
-					'a' - 'a': {
-						child: [28]*pathTrie{
-							'r' - 'a': {
-								name: "/bar",
-								leaf: textHandler("kylelemons.net /bar"),
-							},
-						},
-					},
-					'o' - 'a': {
-						child: [28]*pathTrie{
-							'o' - 'a': {
-								name: "/boo",
-								leaf: textHandler("kylelemons.net /boo"),
-							},
-						},
-					},
-				},
-			},
-		},
-		leaf: textHandler("kylelemons.net /"),
-	}
-
-	TestTrie = domainTrie{
-		child: []*domainPiece{{
-			piece: "com",
-			child: domainTrie{
-				leaf: com,
-			},
-		}, {
-			piece: "net",
-			child: domainTrie{
-				child: []*domainPiece{{
-					piece: "kylelemons",
-					child: domainTrie{
-						leaf: kylelemons_net,
-					},
-				}},
-			},
-		}},
-		leaf: root,
-	}
-	DomainFindTests = []struct {
-		domain string
-		count  int
-		found  *pathTrie
-	}{
-		{
-			domain: "",
-			count:  0,
-			found:  root,
-		},
-		{
-			domain: "example.com.",
-			count:  1,
-			found:  com,
-		},
-		{
-			domain: "kylelemons.net",
-			count:  2,
-			found:  kylelemons_net,
-		},
-		{
-			domain: "sub.kylelemons.net",
-			count:  2,
-			found:  kylelemons_net,
-		},
-	}
-	PathFindTests = []struct {
-		base  *pathTrie
-		path  string
-		count int
-		dir   bool
-		strip bool
-		name  string
-		found string
-	}{
-		{
-			base:  root,
-			path:  "/",
-			count: 0,
-			dir:   true,
-			found: "root /",
-		},
-		{
-			base:  root,
-			path:  "/foo",
-			count: 4,
-			name:  "/foo",
-			found: "root /foo",
-		},
-		{
-			base:  root,
-			path:  "/foo/",
-			count: 4,
-			dir:   true,
-			strip: true,
-			name:  "/foo",
-			found: "root /foo/",
-		},
-		{
-			base:  root,
-			path:  "/foo!",
-			count: 5,
-			name:  "/foo!",
-			found: "root /foo!",
-		},
-		{
-			base:  root,
-			path:  "/foo/bar",
-			count: 4,
-			dir:   true,
-			strip: true,
-			name:  "/foo",
-			found: "root /foo/",
-		},
-		{
-			base:  kylelemons_net,
-			path:  "/",
-			count: 0,
-			dir:   true,
-			found: "kylelemons.net /",
-		},
-		{
-			base:  kylelemons_net,
-			path:  "/ba",
-			count: 0,
-			dir:   true,
-			found: "kylelemons.net /",
-		},
-		{
-			base:  kylelemons_net,
-			path:  "/bo",
-			count: 0,
-			dir:   true,
-			found: "kylelemons.net /",
-		},
-		{
-			base:  kylelemons_net,
-			path:  "/bar/bar",
-			count: 4,
-			name:  "/bar",
-		},
-		{
-			base:  kylelemons_net,
-			path:  "/BAR",
-			count: 4,
-			name:  "/bar",
-		},
-	}
-)
-
-func TestDomainFind(t *testing.T) {
-	for _, test := range DomainFindTests {
-		count, found := TestTrie.find(splitFields(test.domain, "."))
-		if got, want := count, test.count; got != want {
-			t.Errorf("find(%q).count = %v, want %v", test.domain, got, want)
-		}
-		if got, want := found, test.found; got != want {
-			t.Errorf("find(%q).trie = %p, want %p", test.domain, got, want)
-		}
-	}
-}
-
-func BenchmarkDomainFind(b *testing.B) {
-	var domains [][]string
-	for _, test := range DomainFindTests {
-		domains = append(domains, splitFields(test.domain, "."))
-	}
+func benchMux(b *testing.B, mux http.Handler) {
 	b.ReportAllocs()
 	b.ResetTimer()
-	for i := 0; i < b.N; i += len(domains) {
-		for _, s := range domains {
-			TestTrie.find(s)
-		}
-	}
-}
-
-func TestPathFind(t *testing.T) {
-	for _, test := range PathFindTests {
-		count, dir, strip, name, found := test.base.find(test.path)
-		if got, want := count, test.count; got != want {
-			t.Errorf("find(%q).count = %v, want %v", test.path, got, want)
-		}
-		if got, want := dir, test.dir; got != want {
-			t.Errorf("find(%q).dir = %v, want %v", test.path, got, want)
-		}
-		if got, want := strip, test.strip; got != want {
-			t.Errorf("find(%q).strip = %v, want %v", test.path, got, want)
-		}
-		if got, want := name, test.name; got != want {
-			t.Errorf("find(%q).name = %v, want %v", test.path, got, want)
-		}
-		if got, want := found, textHandler(test.found); want != "" && got != want {
-			t.Errorf("find(%q).found = %q, want %q", test.path, got, want)
-		}
-	}
-}
-
-func BenchmarkPathFind(b *testing.B) {
-	var bases []*pathTrie
-	var paths []string
-	for _, test := range PathFindTests {
-		bases = append(bases, test.base)
-		paths = append(paths, test.path)
-	}
-	b.ReportAllocs()
-	b.ResetTimer()
-	for i := 0; i < b.N; i += len(bases) {
-		for j := range bases {
-			bases[j].find(paths[j])
-		}
-	}
-}
-
-func makeBenchmarkData() (hosts, paths []string, reqs []*http.Request) {
-	for _, d := range DomainFindTests {
-		if d.found == nil {
-			continue
-		}
-		for _, p := range PathFindTests {
-			if p.found == "" || p.base != d.found {
-				continue
-			}
-
-			req, err := http.NewRequest("GET", p.path, nil)
-			if err != nil {
-				panic(err)
-			}
-			req.Header.Set("Host", d.domain)
-			req.Host = d.domain
-
-			hosts = append(hosts, d.domain)
-			paths = append(paths, p.path)
-			reqs = append(reqs, req)
-		}
-	}
-	return
-}
-
-func TestBenchmarkData(t *testing.T) {
-	hosts, paths, reqs := makeBenchmarkData()
-	mux := http.NewServeMux()
-	for i := range hosts {
-		pattern := hosts[i] + paths[i]
-		mux.Handle(pattern, textHandler(pattern))
-	}
-	trie := &ServeMux{
-		domain: TestTrie,
-	}
-	muxes := []http.Handler{
-		mux, trie,
-	}
-	for _, req := range reqs {
-		for _, h := range muxes {
+	reqs := []*http.Request{}
+	for i := 0; i < b.N; i += len(reqs) {
+		for _, req := range reqs {
 			rw := httptest.NewRecorder()
-			h.ServeHTTP(rw, req)
-			if rw.Code != http.StatusFound {
-				t.Errorf("%T %q %q: HTTP %d", h, req.Host, req.URL.Path, rw.Code)
-			}
-		}
-	}
-}
-
-func BenchmarkHTTPMux(b *testing.B) {
-	hosts, paths, reqs := makeBenchmarkData()
-	mux := http.NewServeMux()
-	for i := range hosts {
-		pattern := hosts[i] + paths[i]
-		mux.Handle(pattern, textHandler(pattern))
-	}
-	rw := httptest.NewRecorder()
-	b.ReportAllocs()
-	b.ResetTimer()
-	for i := 0; i < b.N; i += len(reqs) {
-		for _, req := range reqs {
-			mux.ServeHTTP(rw, req)
-		}
-	}
-}
-
-func BenchmarkServeMux(b *testing.B) {
-	_, _, reqs := makeBenchmarkData()
-	mux := &ServeMux{
-		domain: TestTrie,
-	}
-	rw := httptest.NewRecorder()
-	b.ReportAllocs()
-	b.ResetTimer()
-	for i := 0; i < b.N; i += len(reqs) {
-		for _, req := range reqs {
 			mux.ServeHTTP(rw, req)
 		}
 	}
